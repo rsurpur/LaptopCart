f7b78911d997a6ac0717bc8ef8124af509b0ea6a
RAMESH <RAMESH@LAPTOP-3ON7IKVT>
2026-07-16 23:27:19 +0530

    Add project files.

---
+++ LaptopCart/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using LaptopCart.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaptopCart.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            //pass the data from controller to view using ViewBag
+            ViewBag.connection = _configuration["ConnectionStrings"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
